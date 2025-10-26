77be95d594f427a04a1b4b8231ce9e8347e4f26b
Marko Mackic <markomackic@gmail.com>
2025-10-26 02:27:50 +0100

    enough for the day

--- brcom.sh
+++ brcom.sh
@@ -2,8 +2,8 @@
 export BR2_EXTERNAL=$(pwd)/buildroot_ext
 export BR2_DEFCONFIG=$(pwd)/buildroot_ext/configs/hlk7621_defconfig
 #export BR2_LINUX_KERNEL_CUSTOM_CONFIG_FILE=$(pwd)/buildroot_ext/hilink/mt7621/linux.config
-
+#export CPPFLAGS="-std=c++11"
 
 pushd buildroot > /dev/null
-"$@"
+make "$@"
 popd > /dev/null
